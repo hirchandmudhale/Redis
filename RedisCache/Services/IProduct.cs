0e31880616d067e572ee97e70f2eac644837ee77
Hiracand Mudhale <hirachand.mudhale@outlook.com>
2024-11-17 22:52:18 +0530

    Updated the code with implementation of service repos for the prodct

---
+++ RedisCache/Services/IProduct.cs
@@ -0,0 +1,16 @@
+﻿using RedisCache.Model;
+
+namespace RedisCache.Services
+{
+    public interface IProduct
+    {
+        public Product GetDataById(int id);
+        public List<Product>GetAll();
+
+        public IEnumerable<Product> Get();
+
+
+
+
+
+}
